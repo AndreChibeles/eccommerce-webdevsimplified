fb53592521c0d94987da4990803dee12a6f19c4a
AndreChibeles <andrechibeles@gmail.com>
2025-01-27 14:45:30 +0000

    27-01-2025 commit

--- src/app/admin/_actions/product.ts
+++ src/app/admin/_actions/product.ts
@@ -3,7 +3,7 @@
 import { z } from "zod";
 import fs from "fs/promises";
 import db from "@/db/db";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { File } from "buffer";
 
 const fileSchema = z.instanceof(File, {
@@ -22,7 +22,7 @@ const addSchema = z.object({
   image: imageSchema.refine((file) => file.size > 0, "Required"),
 });
 
-export async function addProduct(formData: FormData) {
+export async function addProduct(prevState: unknown, formData: FormData) {
   const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
 
   if (result.success === false) {
@@ -44,6 +44,7 @@ export async function addProduct(formData: FormData) {
 
   await db.product.create({
     data: {
+      isAvailableForPurchase: false,
       name: data.name,
       description: data.description,
       priceInCents: data.priceInCents,
@@ -53,4 +54,21 @@ export async function addProduct(formData: FormData) {
   });
 
   redirect("/admin/products");
+}
+
+export async function toggleProductAvailability(
+  id: string,
+  isAvailableForPurchase: boolean
+) {
+  await db.product.update({
+    where: { id },
+    data: {
+      isAvailableForPurchase,
+    },
+  });
+}
+
+export async function deleteProduct(id: string) {
+  const product = await db.product.delete({ where: { id } });
+  if (product === null) return notFound();
 }
